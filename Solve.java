8805c8c0019ba20dd27fe43b85d12bee19de9d3d
Sena <Sena@ARMAPRCCIFOD574.FormacionSena.RED>
2024-07-25 10:29:34 -0500

    primer

---
+++ Solve.java
@@ -0,0 +1,42 @@
+public class Solve {
+    public static void main(String[] args) {
+        
+    
+    // 1 resultado de la operación
+    double x = (double) 5.89 + (double) 3.45;
+    System.out.println(x);
+
+    // 2 combertir c a su valor ASCII
+        char c = 'A';
+        int asciiValor = (int) c;
+        System.out.println ("El valor de ASCII de' " + c + "'es:'" + asciiValor);
+
+
+    // 3 valor a imprimir
+    double d = 100.04;
+    long l = (long)d;
+        int i = (int)l;
+    System.out.println(i);
+
+    // 4 convertir String que contiene un numero
+
+      String numeroComoString = "123"; 
+        int numeroComoInt = Integer.parseInt(numeroComoString); 
+        
+         System.out.println("El número como int es: " + numeroComoInt);
+      
+    // 5 resultado 
+   
+
+
+    // 6 convertir 65 en char 
+         int numero = 65; // Número a convertir en un carácter
+         char caracter = (char) numero; // Conversión de int a char
+    
+        System.out.println("El carácter correspondiente al número " + numero + " es: " + caracter);
+        
+        
+        // 7 asignar long a un int casteo 
+
+}
+}
